e29ba12ef607657d037c812760d666273e659851
Iggy <ignatiusasikinn@gmail.com>
2022-12-20 00:39:20 -0500

    Update filecategorized.ps1

--- filecategorized.ps1
+++ filecategorized.ps1
@@ -1,10 +1,10 @@
 # Set the path to the directory where the files are located
 $directoryPath = "C:\path\to\directory"
 
-# Set the extensions to group the files by
-$extensions = @(".doc", ".docx", ".xls", ".xlsx", ".pdf")
+# Set extensions
+$extensions = @(".doc", ".docx", ".xls", ".xlsx", ".pdf", ".jpg")
 
-# Loop through each extension
+# Loop through extension
 foreach ($ext in $extensions) {
     # Get a list of all files with the current extension in the specified directory
     $files = Get-ChildItem -Path $directoryPath -Filter "*$ext"
